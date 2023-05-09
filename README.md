efd99963ae9baaac02ad45bd09f0031ff5dfe7c0
Aylin Rodriguez <aylin@nocturno.com.uy>
2023-05-09 17:36:44 +0200

    release version 0.1.0
    added livereload, multiple build modes, non minified local version

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # mini-html-server
 Compact node.js server for HTML/JS/SASS.
 
-It compiles SASS files into one single min css, it also minify and merge all JavaScripts files. Moves everything into a distribution directory and launches a local server at a defined port in gulpfile.js
+node@11.4.0
+npm@6.7.0
 
-GULP v11.4.0
+It can compiles SASS files into one single css, it also minify and merge all JavaScripts files. 
+Moves everything into a local directory and launches a local server
+
+It uses browser-sync for livereload, it be configured for sync navigation and interaction between multiple browsers
+
+Can also build minified or non minified version of your code
+
+After that just move into a server
+
 
 # how to use
 
@@ -12,14 +21,31 @@ Install node dependencies
 npm install
 ```
 
-To launch just use gulp default task
+To launch just use
 ```
-gulp local
+npm run dev
 ```
+By default it runs at http://localhost:3030 
 Customize localhost port in gulpfile.js
 
 
-Removes the entire distribution folder
+To build use
 ```
-gulp clean
+npm run build
+```
+
+To build minified version
+```
+npm run build-min
+```
+
+
+Removes the entire local folder
+```
+npm run clean
+```
+
+Removes the entire build folder
+```
+npm run prebuild
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,112 @@
 var gulp = require('gulp');
-var http = require('http');
 var min = require('gulp-uglify');
 var sass = require('gulp-sass');
 var minCss = require('gulp-minify-css');
 var clean = require('gulp-clean');
-var gls = require('gulp-live-server');
 var htmlmin = require('gulp-htmlmin');
 var concat = require('gulp-concat');
-
-//specify local port for testing
-const port = 3030
-
-
-gulp.task('compile',['clean'], function() {
-    gulp.start('html','watch-html','sass','watch-sass','javascript','watch-js');
-});
-
-gulp.task('watch-sass', function(){
-    gulp.watch('src/**/*.scss', ['sass'])
-});
-gulp.task('watch-html', function(){
-    gulp.watch('src/**/*.html', ['html'])
-});
-gulp.task('watch-js', function(){
-    gulp.watch('src/**/*.js', ['javascript'])
-});
+var browserSync = require('browser-sync').create();
 
 
 
 gulp.task('sass', function(){
-    gulp.src('src/sass/*.scss')
+    gulp.src('src/sass/style.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(concat("style.css"))
-        .pipe(minCss())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('local/css'));
 });
 
 gulp.task('html', function(){
     gulp.src('src/*.html')
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('local'))
 });
 
 gulp.task('javascript', function(){
     gulp.src('src/js/*.js')
         .pipe(concat("scripts.js"))
-        .pipe(min())
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('local/js'))
+});
+
+gulp.task('watch-sass', function () {
+    gulp.watch('src/**/*.scss', ['sass'])
+});
+gulp.task('watch-html', function () {
+    gulp.watch('src/**/*.html', ['html'])
+});
+gulp.task('watch-js', function () {
+    gulp.watch('src/**/*.js', ['javascript'])
 });
 
 
 
-gulp.task('http-server', function(){
-    var server = gls.static('dist', port);
-    server.start();
+
+
+gulp.task('compile', ['clean'], function () {
+    gulp.start('html', 'watch-html', 'sass', 'watch-sass', 'javascript', 'watch-js');
 });
 
-gulp.task('clean', function(){
-    return gulp.src('dist', {read: false})
+
+
+
+
+gulp.task('build', ['prebuild'], function () {
+
+        gulp.src('src/*.html')
+            .pipe(gulp.dest('build'));
+
+        gulp.src('src/sass/*.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('build/css'));
+
+        gulp.src('src/js/*.js')
+            .pipe(concat("scripts.js"))
+            .pipe(gulp.dest('build/js'));
+        
+        console.log('================================================\nNon minified version ready in /build \n================================================')
+
+});
+
+// use build-min a minified version
+
+gulp.task('build-min', ['prebuild'], function () {
+
+        gulp.src('src/*.html')
+            .pipe(htmlmin({ collapseWhitespace: true }))
+            .pipe(gulp.dest('build'));
+
+        gulp.src('src/sass/*.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(concat("style.css"))
+            .pipe(minCss())
+            .pipe(gulp.dest('build/css'));
+
+        gulp.src('src/js/*.js')
+            .pipe(concat("scripts.js"))
+            .pipe(min())
+            .pipe(gulp.dest('build/js'));
+
+        console.log('================================================\nReady in /build \n================================================')
+
+});
+
+gulp.task('local', ['compile'], function() {
+    browserSync.init({
+        server: {
+            baseDir: 'local'
+        },
+        watch: true,
+        port: 3030,
+        open: false,
+        ghostMode: false
+    })
+});
+
+
+
+gulp.task('clean', function () {
+    return gulp.src('local', { read: false })
         .pipe(clean());
 });
-
-gulp.task('local', ['compile','http-server'], function() {
-    console.log('================================================\n=Ready in http://localhost:'+port+'\n================================================')
+gulp.task('prebuild', function () {
+    return gulp.src('build', { read: false })
+        .pipe(clean());
 });
